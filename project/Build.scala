7a2950fbb62db1b1f32938f7a3c251a9b40f39a8
cgreen <casey@actioniq.co>
2016-01-07 17:27:58 -0500

    set version

--- project/Build.scala
+++ project/Build.scala
@@ -13,7 +13,7 @@ object ScalavroBuild extends Build {
 
   val ORGANIZATION = "co.actioniq"
   val PROJECT_NAME = "scalavro"
-  val PROJECT_VERSION = "0.6.3-SNAPSHOT"
+  val PROJECT_VERSION = "0.6.3-48fb55d55fc79e1a4894697092014fb559930755"
   val SCALA_VERSION = "2.10.3"
 
 
